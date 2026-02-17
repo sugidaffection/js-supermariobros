9fcfa4da30c4c692227eb4691b3b76d1123b17fe
Sugi <sugidaffection@gmail.com>
2026-02-17 13:41:29 +0700

    Refactor game loop to fixed-step simulation and decoupled rendering

--- game.js
+++ game.js
@@ -32,70 +32,74 @@ class Scene {
 		this.sound.play()
 	}
 
-	play(){
-		this.ctx.clearRect(0,0,this.w,this.h)
-		this.tilemap.render(this.ctx)
-		this.mario.render(this.ctx)
-		if(this.loop){
-			this.mario.update(1/60)
-			if(this.mario.controller.right){
-				if(this.tilemap.x < 1200 && this.mario.rect.right > this.w / 2){
-					this.tilemap.update(this.mario.vel.x)
-					this.tilemap.x += 1
-				}else{
-					this.mario.rect.x += this.mario.vel.x
-				}
-			}else if(this.mario.controller.left){
-				if(this.mario.vel.x < 0){
-					this.mario.rect.x += this.mario.vel.x
-				}
-			}
-
-			if(this.mario.rect.left < 0){
-				this.mario.rect.x = 0
-			}
-
-			if(this.mario.rect.right > this.w){
-				this.mario.rect.x = this.w - this.mario.rect.w
-			}
-
-			this.tilemap.solidsprites.forEach(obj => {
-				if(this.mario.rect.bottom + this.mario.vel.y > obj.rect.top &&
-					this.mario.rect.top < obj.rect.top &&
-					this.mario.rect.left + 1 < obj.rect.right &&
-					this.mario.rect.right - 1 > obj.rect.left){
-						this.mario.vel.y = 0
-						this.mario.rect.y = obj.rect.top - this.mario.rect.h
-						this.mario.ground = true
-					}
-				else if(this.mario.rect.top + this.mario.vel.y < obj.rect.bottom &&
-					this.mario.rect.bottom > obj.rect.bottom &&
-					this.mario.rect.left + 1 < obj.rect.right &&
-					this.mario.rect.right - 1 > obj.rect.left){
-						this.mario.rect.y = obj.rect.bottom
-						this.mario.vel.y = 1
-					}
-				if(this.mario.rect.right > obj.rect.left &&
-					this.mario.rect.left < obj.rect.left &&
-					this.mario.rect.top < obj.rect.bottom &&
-					this.mario.rect.bottom > obj.rect.top){
-						this.mario.rect.x = obj.rect.left - this.mario.rect.w
-						this.mario.vel.x = 0
-					}
-				else if(this.mario.rect.left < obj.rect.right &&
-					this.mario.rect.right > obj.rect.right &&
-					this.mario.rect.top < obj.rect.bottom &&
-					this.mario.rect.bottom > obj.rect.top){
-						this.mario.rect.x = obj.rect.right
-						this.mario.vel.x = 0
-					}
-			})
-
-			this.mario.rect.y += this.mario.vel.y
-
+	update(dt){
+		if(!this.loop){
+			return
 		}
 
-		if(this.tilemap.x == 1200){
+		this.mario.update(dt)
+		const deltaX = this.mario.vel.x * dt
+		const deltaY = this.mario.vel.y * dt
+
+		if(this.mario.controller.right){
+			if(this.tilemap.x < 1200 && this.mario.rect.right > this.w / 2){
+				this.tilemap.update(deltaX)
+				this.tilemap.x += Math.max(0, deltaX)
+			}else{
+				this.mario.rect.x += deltaX
+			}
+		}else if(this.mario.controller.left){
+			if(deltaX < 0){
+				this.mario.rect.x += deltaX
+			}
+		}
+
+		if(this.mario.rect.left < 0){
+			this.mario.rect.x = 0
+		}
+
+		if(this.mario.rect.right > this.w){
+			this.mario.rect.x = this.w - this.mario.rect.w
+		}
+
+		this.mario.rect.update()
+
+		this.tilemap.solidsprites.forEach(obj => {
+			if(this.mario.rect.bottom + deltaY > obj.rect.top &&
+				this.mario.rect.top < obj.rect.top &&
+				this.mario.rect.left + 1 < obj.rect.right &&
+				this.mario.rect.right - 1 > obj.rect.left){
+					this.mario.vel.y = 0
+					this.mario.rect.y = obj.rect.top - this.mario.rect.h
+					this.mario.ground = true
+			}
+			else if(this.mario.rect.top + deltaY < obj.rect.bottom &&
+				this.mario.rect.bottom > obj.rect.bottom &&
+				this.mario.rect.left + 1 < obj.rect.right &&
+				this.mario.rect.right - 1 > obj.rect.left){
+					this.mario.rect.y = obj.rect.bottom
+					this.mario.vel.y = 1
+			}
+			if(this.mario.rect.right > obj.rect.left &&
+				this.mario.rect.left < obj.rect.left &&
+				this.mario.rect.top < obj.rect.bottom &&
+				this.mario.rect.bottom > obj.rect.top){
+					this.mario.rect.x = obj.rect.left - this.mario.rect.w
+					this.mario.vel.x = 0
+			}
+			else if(this.mario.rect.left < obj.rect.right &&
+				this.mario.rect.right > obj.rect.right &&
+				this.mario.rect.top < obj.rect.bottom &&
+				this.mario.rect.bottom > obj.rect.top){
+					this.mario.rect.x = obj.rect.right
+					this.mario.vel.x = 0
+			}
+		})
+
+		this.mario.rect.y += this.mario.vel.y * dt
+		this.mario.rect.update()
+
+		if(this.tilemap.x >= 1200){
 			this.win = true
 		}
 
@@ -103,6 +107,17 @@ class Scene {
 			this.idx = 1
 			this.load_sound()
 		}
+	}
+
+	render(interpolationAlpha=0){
+		this.ctx.clearRect(0,0,this.w,this.h)
+		this.tilemap.render(this.ctx)
+		this.mario.render(this.ctx)
+		return interpolationAlpha
+	}
+
+	play(interpolationAlpha=0){
+		this.render(interpolationAlpha)
 	}
 
 	stop(){
@@ -120,11 +135,32 @@ class Game {
 		canvas.style.border = '1px solid #000'
 		const ctx = canvas.getContext('2d')
 		this.scene = new Scene(ctx,w,h)
+		this.lastTime = 0
+		this.accumulator = 0
+		this.fixedDt = 1/60
+		this.maxFrameDelta = 0.25
 	}
 
-	run(s){
-		this.scene.play()
-		requestAnimationFrame((dt)=>this.run(s))
+	run(timestamp){
+		const now = timestamp / 1000
+		if(!this.lastTime){
+			this.lastTime = now
+		}
+
+		let frameDelta = now - this.lastTime
+		this.lastTime = now
+		frameDelta = Math.min(frameDelta, this.maxFrameDelta)
+		this.accumulator += frameDelta
+
+		while(this.accumulator >= this.fixedDt){
+			this.scene.update(this.fixedDt)
+			this.accumulator -= this.fixedDt
+		}
+
+		const interpolationAlpha = this.accumulator / this.fixedDt
+		this.scene.render(interpolationAlpha)
+
+		requestAnimationFrame((dt)=>this.run(dt))
 	}
 
 }
